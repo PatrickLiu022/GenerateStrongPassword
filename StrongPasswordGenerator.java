fe77476e6f6f91b956f30587c1ed8119ef807228
patrickliu022 <patrickliu022@gmail.com>
2020-08-21 02:49:29 -0700

    Fixed password after exlcusion

--- StrongPasswordGenerator.java
+++ StrongPasswordGenerator.java
@@ -1,4 +1,4 @@
-/* 
+/*
 Patrick Liu
 8/16/20
 */
@@ -10,14 +10,14 @@ public class StrongPasswordGenerator {
 
    private int size;                         //records password size
    private ArrayList<Character> password;    //contains generated password
-   
+
    private static final int ASCII = 93;      //this alogirthm will use 93 kinds of ASCII characters
-   
+
    //post: new password length is 0
    public StrongPasswordGenerator(){
       size = 0;
    }
-   
+
    //pre:  A password is generated
    //post: counts the characters in the given password and
    //      a stores each character individually. Stores the
@@ -29,17 +29,17 @@ public class StrongPasswordGenerator {
          size++;
       }
    }
-   
+
    //post: gives the size of the password
    public int size(){
       return size;
    }
-   
-   //post: returns the password 
+
+   //post: returns the password
    public ArrayList<Character> get(){
       return password;
    }
-   
+
    //pre:  @exception IllegalArgumentException if the
    //      length of desired password is less than or equal
    //      to zero
@@ -48,38 +48,40 @@ public class StrongPasswordGenerator {
    public void set(int length){
       int count = 0;
       Random r = new Random();
-      
+
       if (length <= 0){
          throw new IllegalArgumentException();
       }
-      
+
       int num = r.nextInt(ASCII) + 33;
       while(count < length){
          char letter = (char) num;
          password.add(letter);
          num = r.nextInt(ASCII) + 33;
          count++;
-      }  
+      }
    }
-   
+
    //pre:  characters that are not desired in the new password have been
    //      specified by the user
-   //psot: Checks if the characters the user wants to exclude exists in the 
+   //psot: Checks if the characters the user wants to exclude exists in the
    //      new generated password and removes it if it exists.
    public ArrayList<Character> exclude(StrongPasswordGenerator other){
+       int count = 0;
       for (Iterator<Character> itr = password.iterator();itr.hasNext(); ){
          Character c = itr.next();
          for (Iterator<Character> itr1 = other.password.iterator(); itr1.hasNext(); ){
             Character o = itr1.next();
             if (c.equals(o)){
                itr.remove();
+               count++;
             }
          }
       }
-      
+
       return password;
    }
-   
+
    //post: Takes the array of individual characters in the password
    //      and stores them together in a string
    public String toString(){
