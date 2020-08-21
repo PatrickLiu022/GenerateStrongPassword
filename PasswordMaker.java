3f78256c0ebcb6f2aa3fb7cbe3f58b4071b83a55
patrickliu022 <patrickliu022@gmail.com>
2020-08-21 12:18:49 -0700

    Update PasswordMaker.java

--- PasswordMaker.java
+++ PasswordMaker.java
@@ -1,6 +1,4 @@
 /*
-Patrick Liu 
-08/16
 */
 
 import java.util.*;
@@ -9,57 +7,57 @@ public class PasswordMaker {
 
    public static void main(String[] args){
       Scanner console = new Scanner(System.in);
-      boolean continueGenerate = true;
-      intro();
-      
-      String exclude = console.nextLine();
-      System.out.print("How many passwords to generate? ");
-      int num = console.nextInt();
-      int count = 0;
-      String generate = "";
       StrongPasswordGenerator strongPass = new StrongPasswordGenerator("");
       
-      while(count < num){
-         generatePassword(console, strongPass, exclude);
-         count++;
+      intro();
+      System.out.print("End program? (y/n): ");
+      String start = console.nextLine();
+      
+      while(!start.startsWith("y")){
+         Scanner redo = new Scanner(System.in);
+         System.out.println("Character exclusion:");
+         System.out.println("type no for no exclusion, yes to exclude characters");
+         System.out.print("Exclude characters? ");
+         String exclude = redo.nextLine();
+         if (exclude.toLowerCase().startsWith("y")) {
+            excludePassword(strongPass, exclude);
+         } else {
+            regularPassword(console, strongPass);
+         }
+         System.out.print("End program? (y/n): ");
+         start = redo.nextLine();
       }
    }
    
    public static void intro(){
       System.out.println("Welcome to the Strong Password Generator");
       System.out.println("This program will help you create a strong password");
-      System.out.println("Use this anytime you need a password change");
-      System.out.println("Exclude any characters?");
-      System.out.println("type: no = no exclusion, yes = exclude characters");
-      System.out.print("exclude characters? ");
+      System.out.println("Use this anytime you need a password change\n");
+
    }
    
-   public static void generatePassword(Scanner console, StrongPasswordGenerator strongPass, String exclude){
-      ArrayList<Character> charPass = new ArrayList<Character>();
+   public static void excludePassword(StrongPasswordGenerator strongPass, String exclude){
       StrongPasswordGenerator excluder;
-      String excludedChars = "";
-      int length = 0;
+      Scanner consoleNew = new Scanner(System.in);
       
-      if(!exclude.startsWith("no")){
-         System.out.print("Which characters to exclude? ");
-         excludedChars = console.nextLine();
-         excluder = new StrongPasswordGenerator(excludedChars);
+      System.out.print("Which characters to exclude? ");
+      String excludeChars = consoleNew.nextLine();
+      excluder = new StrongPasswordGenerator(excludeChars);
+      System.out.print("password length? ");
+      strongPass.set(consoleNew.nextInt());
+      String generatedPass = strongPass.exclude(excluder).toString();
+      generatedPass.replace(" ", ".");
+      System.out.println("Your new strong password is: " + generatedPass);
+      
+      
+   }
+   
+   public static void regularPassword(Scanner console, StrongPasswordGenerator strongPass){
          System.out.print("password length? ");
-         length = console.nextInt();
-         strongPass.set(length);
-         charPass = strongPass.exclude(excluder);
-         String generatedPass = charPass.toString();
-         System.out.println("Your new strong password is: " + generatedPass);
-      } else {
-         System.out.print("password length? ");
-         length = console.nextInt();
-         strongPass.set(length);
-         
+         strongPass.set(console.nextInt());
          String generatedPass = strongPass.toString();
          generatedPass = generatedPass.replace(" ", ".");
          System.out.println("Your new strong password is " + generatedPass);
-      }
-      
    }
    
 }   
