4d577d7f8b7a67e21a33c8cde8dbe37b9d0183aa
patrickliu022 <patrickliu022@gmail.com>
2020-08-23 01:41:59 -0700

    JAVA LINTING

--- PasswordMaker.java
+++ PasswordMaker.java
@@ -9,82 +9,82 @@ import java.util.*;
 //class prompts user to generate a password, with or without certain
 //characters only.
 public class PasswordMaker {
-   
-   //pre:  prompts user to specify if they want to exclude characters
-   //      and how long they want the password to be. User can end the
-   //      program anytime.
-   //post: prints out the new strong password for the user.
-   public static void main(String[] args){
-      Scanner console = new Scanner(System.in);
-      String start;
-      intro();
-      
-      do {
-         StrongPasswordGenerator strongPass = new StrongPasswordGenerator(""); //new StrongPasswordGenerator object
-         Scanner redo = new Scanner(System.in);
-         System.out.println("Character exclusion:");
-         System.out.println("type no for no exclusion, yes to exclude characters");
-         System.out.print("Exclude characters? ");
-         String exclude = redo.nextLine();
-         if (exclude.toLowerCase().startsWith("y")) {
-            excludePassword(strongPass, exclude);
-         } else {
-            regularPassword(console, strongPass);
-         } 
-         System.out.print("End program? (y/n): ");
-         start = redo.nextLine(); 
-      }  while(!start.startsWith("y"));
-      
-   }
-   
-   
-   //post: prints out an introduction and defines what a strong
-   //      password is for the user
-   public static void intro(){
-      System.out.println("Welcome to the Strong Password Generator");
-      System.out.println("This program will help you create a strong password.");
-      System.out.println("A strong password contains upper and lower case");
-      System.out.println("characters, numerical characters, and symbols.");
-      System.out.println("It also must be at least 6 characters long");
-      System.out.println("Use this program anytime you need a password change\n");
 
-   }
-   
-   //pre:  prompts user to enter which characters to exclude and how long
-   //      they want the password to be. Exception thrown if length < 6.
-   //post: prints out a new strong password for the user.
-   public static void excludePassword(StrongPasswordGenerator strongPass, String exclude){
-      StrongPasswordGenerator excluder;
-      ArrayList<Character> list;
-      Scanner consoleNew = new Scanner(System.in);
-      
-      System.out.print("Which characters to exclude? ");
-      String excludeChars = consoleNew.nextLine();
-      excluder = new StrongPasswordGenerator(excludeChars);
-      try {
-         System.out.print("password length? ");
-         strongPass.set(consoleNew.nextInt());
-      } catch (Exception e){
-         System.out.println("Please enter an integer value greater than 6");
-      }
-      strongPass.exclude(excluder.get());
-      strongPass.toString();
-      System.out.println("Your new strong password is: " + strongPass);
-      
-      
-   }
-   
-   //pre:  prompts user for password length. Exception thrown if length < 6.
-   //post: generates a new strong password.
-   public static void regularPassword(Scanner console, StrongPasswordGenerator strongPass){
-         try {
+    //pre:  prompts user to specify if they want to exclude characters
+    //      and how long they want the password to be. User can end the
+    //      program anytime.
+    //post: prints out the new strong password for the user.
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String start;
+        intro();
+
+        do {
+            StrongPasswordGenerator strongPass = new StrongPasswordGenerator(""); //new StrongPasswordGenerator object
+            Scanner redo = new Scanner(System.in);
+            System.out.println("Character exclusion:");
+            System.out.println("type no for no exclusion, yes to exclude characters");
+            System.out.print("Exclude characters? ");
+            String exclude = redo.nextLine();
+            if (exclude.toLowerCase().startsWith("y")) {
+                excludePassword(strongPass, exclude);
+            } else {
+                regularPassword(console, strongPass);
+            }
+            System.out.print("End program? (y/n): ");
+            start = redo.nextLine();
+        } while (!start.startsWith("y"));
+
+    }
+
+
+    //post: prints out an introduction and defines what a strong
+    //      password is for the user
+    public static void intro() {
+        System.out.println("Welcome to the Strong Password Generator");
+        System.out.println("This program will help you create a strong password.");
+        System.out.println("A strong password contains upper and lower case");
+        System.out.println("characters, numerical characters, and symbols.");
+        System.out.println("It also must be at least 6 characters long");
+        System.out.println("Use this program anytime you need a password change\n");
+
+    }
+
+    //pre:  prompts user to enter which characters to exclude and how long
+    //      they want the password to be. Exception thrown if length < 6.
+    //post: prints out a new strong password for the user.
+    public static void excludePassword(StrongPasswordGenerator strongPass, String exclude) {
+        StrongPasswordGenerator excluder;
+        ArrayList < Character > list;
+        Scanner consoleNew = new Scanner(System.in);
+
+        System.out.print("Which characters to exclude? ");
+        String excludeChars = consoleNew.nextLine();
+        excluder = new StrongPasswordGenerator(excludeChars);
+        try {
+            System.out.print("password length? ");
+            strongPass.set(consoleNew.nextInt());
+        } catch (Exception e) {
+            System.out.println("Please enter an integer value greater than 6");
+        }
+        strongPass.exclude(excluder.get());
+        strongPass.toString();
+        System.out.println("Your new strong password is: " + strongPass);
+
+
+    }
+
+    //pre:  prompts user for password length. Exception thrown if length < 6.
+    //post: generates a new strong password.
+    public static void regularPassword(Scanner console, StrongPasswordGenerator strongPass) {
+        try {
             System.out.print("password length? ");
             strongPass.set(console.nextInt());
-         } catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Please enter an integer value greater than 6");
-         }
-         String generatedPass = strongPass.toString();
-         System.out.println("Your new strong password is " + generatedPass);
-   }
-   
-}   
+        }
+        String generatedPass = strongPass.toString();
+        System.out.println("Your new strong password is " + generatedPass);
+    }
+
+}
--- StrongPasswordGenerator.java
+++ StrongPasswordGenerator.java
@@ -11,117 +11,117 @@ import java.util.*;
 //that will be used as a new password for the user.
 public class StrongPasswordGenerator {
 
-   private int size;                         //records password size.
-   private ArrayList<Character> password;    //contains generated password.
-   private Random r;
+    private int size; //records password size.
+    private ArrayList < Character > password; //contains generated password.
+    private Random r;
 
-   private static final int ASCII = 93;      //will choose from 93 kinds of ASCII characters.
+    private static final int ASCII = 93; //will choose from 93 kinds of ASCII characters.
 
-   //post: new password length is 0
-   public StrongPasswordGenerator(){
-      size = 0;
-   }
+    //post: new password length is 0
+    public StrongPasswordGenerator() {
+        size = 0;
+    }
 
-   //pre:  A password is generated.
-   //post: counts the characters in the given password and
-   //      a stores each character individually. Stores the
-   //      size of the password.
-   public StrongPasswordGenerator(String data){
-      password = new ArrayList<Character>();
-      for (int i = 0; i < data.length(); i++){
-         password.add(data.charAt(i));
-         size++;
-      }
-   }
+    //pre:  A password is generated.
+    //post: counts the characters in the given password and
+    //      a stores each character individually. Stores the
+    //      size of the password.
+    public StrongPasswordGenerator(String data) {
+        password = new ArrayList < Character > ();
+        for (int i = 0; i < data.length(); i++) {
+            password.add(data.charAt(i));
+            size++;
+        }
+    }
 
-   //post: gives the size of the password.
-   public int size(){
-      return size;
-   }
+    //post: gives the size of the password.
+    public int size() {
+        return size;
+    }
 
-   //post: returns the password.
-   public ArrayList<Character> get(){
-      return password;
-   }
+    //post: returns the password.
+    public ArrayList < Character > get() {
+        return password;
+    }
 
-   //pre:  @exception IllegalArgumentException if the
-   //      length of desired password is less than or equal
-   //      to zero.
-   //post: generates random ASCII characters and stores each
-   //      cahracter into a new password.
-   public void set(int length){
-      int count = 0;
-      r = new Random();
+    //pre:  @exception IllegalArgumentException if the
+    //      length of desired password is less than or equal
+    //      to zero.
+    //post: generates random ASCII characters and stores each
+    //      cahracter into a new password.
+    public void set(int length) {
+        int count = 0;
+        r = new Random();
 
-      if (length < 6){
-         throw new IllegalArgumentException();
-      }
+        if (length < 6) {
+            throw new IllegalArgumentException();
+        }
 
-      int num = r.nextInt(ASCII) + 33;
-      while(count < length){
-         char letter = (char) num;
-         password.add(letter);
-         num = r.nextInt(ASCII) + 33;
-         count++;
-      }
-   }
-
-   //pre:  characters that are not desired in the new password have been
-   //      specified by the user.
-   //psot: Checks if the characters the user wants to exclude exists in the
-   //      new generated password and removes it if it exists.
-   public ArrayList<Character> exclude(ArrayList<Character> other){
-      int count = 0;
-      
-      for (int i = 0; i < password.size(); i++){
-         if (other.contains(password.get(i))){
-            password.remove(password.get(i));
-            i--;
+        int num = r.nextInt(ASCII) + 33;
+        while (count < length) {
+            char letter = (char) num;
+            password.add(letter);
+            num = r.nextInt(ASCII) + 33;
             count++;
-         }
-      }
-      
-      exclude(count, other);  
-      return password;
-   }
-   
-   //pre:  the user has entered the characters they want to exclude and
-   //      is stored in an array.
-   //post: generates as many ASCII characters for the new strong password 
-   //      as needed to replace the excluded characters.   
-   private void exclude(int count, ArrayList<Character> other){
-      int val = r.nextInt(ASCII) + 33;
-      
-      for (int i = count; i > 0; i--){
-         do {
-            val = r.nextInt(ASCII) + 33;
-         } while(includes((char) val, other));
-       
-         password.add((char) val);        
-      } 
-   }
-   
-   //pre:  a single character and the array of characters to be excluded
-   //      exists.
-   //post: true if the variable exists in the array, false if the variable
-   //      does not exist.
-   public boolean includes(char val, ArrayList<Character> other){
-      for (int i = 0; i < other.size(); i++){
-         if (other.get(i) == val){
-            return other.get(i) == val;
-         }
-      }
-      return false;
-   } 
-  
-   //post: Takes the array of individual characters in the password
-   //      and stores them together in a string.
-   public String toString(){
-      String strPass = "";
-      for (char c : password){
-         strPass += c;
-      }
-      return strPass;
-   }
-    
+        }
+    }
+
+    //pre:  characters that are not desired in the new password have been
+    //      specified by the user.
+    //psot: Checks if the characters the user wants to exclude exists in the
+    //      new generated password and removes it if it exists.
+    public ArrayList < Character > exclude(ArrayList < Character > other) {
+        int count = 0;
+
+        for (int i = 0; i < password.size(); i++) {
+            if (other.contains(password.get(i))) {
+                password.remove(password.get(i));
+                i--;
+                count++;
+            }
+        }
+
+        exclude(count, other);
+        return password;
+    }
+
+    //pre:  the user has entered the characters they want to exclude and
+    //      is stored in an array.
+    //post: generates as many ASCII characters for the new strong password 
+    //      as needed to replace the excluded characters.   
+    private void exclude(int count, ArrayList < Character > other) {
+        int val = r.nextInt(ASCII) + 33;
+
+        for (int i = count; i > 0; i--) {
+            do {
+                val = r.nextInt(ASCII) + 33;
+            } while (includes((char) val, other));
+
+            password.add((char) val);
+        }
+    }
+
+    //pre:  a single character and the array of characters to be excluded
+    //      exists.
+    //post: true if the variable exists in the array, false if the variable
+    //      does not exist.
+    public boolean includes(char val, ArrayList < Character > other) {
+        for (int i = 0; i < other.size(); i++) {
+            if (other.get(i) == val) {
+                return other.get(i) == val;
+            }
+        }
+        return false;
+    }
+
+    //post: Takes the array of individual characters in the password
+    //      and stores them together in a string.
+    public String toString() {
+        String strPass = "";
+        for (char c: password) {
+            strPass += c;
+        }
+        return strPass;
+    }
+
 }
