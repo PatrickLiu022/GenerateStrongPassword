49eb2f8ca63a892b4d195fee4c807cb64307e766
patrickliu022 <patrickliu022@gmail.com>
2020-08-24 20:19:00 -0700

    PrintStream feature

--- PasswordMaker.java
+++ PasswordMaker.java
@@ -5,6 +5,7 @@ PasswordMaker is functional: 8/22/2020
 */
 
 import java.util.*;
+import java.io.*;
 
 //class prompts user to generate a password, with or without certain
 //characters only.
@@ -14,25 +15,33 @@ public class PasswordMaker {
     //      and how long they want the password to be. User can end the
     //      program anytime.
     //post: prints out the new strong password for the user.
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
         Scanner console = new Scanner(System.in);
+        PrintStream output = new PrintStream(new File("passwordFile.txt"));
         String start;
+        String password = "";
+        int count = 0;
+        
         intro();
 
         do {
             StrongPasswordGenerator strongPass = new StrongPasswordGenerator(""); //new StrongPasswordGenerator object
-            Scanner redo = new Scanner(System.in);
+            output = System.out;
             System.out.println("Character exclusion:");
             System.out.println("type no for no exclusion, yes to exclude characters");
             System.out.print("Exclude characters? ");
-            String exclude = redo.nextLine();
+            String exclude = console.next();
             if (exclude.toLowerCase().startsWith("y")) {
-                excludePassword(strongPass, exclude);
+                password = excludePassword(strongPass, console, exclude);
+                output.println("password " + count + ":\t" + password);
             } else {
-                regularPassword(console, strongPass);
+                password = regularPassword(strongPass, console);
+                output.println("password " + count + ":\t" + password);
             }
+            System.out.println("Your new strong password is: " + password);
+            count++;
             System.out.print("End program? (y/n): ");
-            start = redo.nextLine();
+            start = console.next();
         } while (!start.startsWith("y"));
 
     }
@@ -53,38 +62,40 @@ public class PasswordMaker {
     //pre:  prompts user to enter which characters to exclude and how long
     //      they want the password to be. Exception thrown if length < 6.
     //post: prints out a new strong password for the user.
-    public static void excludePassword(StrongPasswordGenerator strongPass, String exclude) {
+    public static String excludePassword(StrongPasswordGenerator strongPass, 
+                                         Scanner console, String exclude){
         StrongPasswordGenerator excluder;
         ArrayList < Character > list;
-        Scanner consoleNew = new Scanner(System.in);
+        //Scanner consoleNew = new Scanner(System.in);
 
         System.out.print("Which characters to exclude? ");
-        String excludeChars = consoleNew.nextLine();
+        String excludeChars = console.next();
         excluder = new StrongPasswordGenerator(excludeChars);
-        try {
-            System.out.print("password length? ");
-            strongPass.set(consoleNew.nextInt());
-        } catch (Exception e) {
-            System.out.println("Please enter an integer value greater than 6");
-        }
-        strongPass.exclude(excluder.get());
-        strongPass.toString();
-        System.out.println("Your new strong password is: " + strongPass);
-
-
-    }
-
-    //pre:  prompts user for password length. Exception thrown if length < 6.
-    //post: generates a new strong password.
-    public static void regularPassword(Scanner console, StrongPasswordGenerator strongPass) {
         try {
             System.out.print("password length? ");
             strongPass.set(console.nextInt());
         } catch (Exception e) {
             System.out.println("Please enter an integer value greater than 6");
         }
-        String generatedPass = strongPass.toString();
-        System.out.println("Your new strong password is " + generatedPass);
+        strongPass.exclude(excluder.get());
+        //strongPass.toString();
+        return strongPass.toString();
+        //System.out.println("Your new strong password is: " + strongPass);
+
+
+    }
+
+    //pre:  prompts user for password length. Exception thrown if length < 6.
+    //post: generates a new strong password.
+    public static String regularPassword(StrongPasswordGenerator strongPass, Scanner console) {
+        try {
+            System.out.print("password length? ");
+            strongPass.set(console.nextInt());
+        } catch (Exception e) {
+            System.out.println("Please enter an integer value greater than 6");
+        }
+        return strongPass.toString();
+        //System.out.println("Your new strong password is " + generatedPass);
     }
     
 }
